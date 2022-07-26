6b5102db4a8d4f8f34601ab37c4efbf63bc7f624
az-marques <85958771+az-marques@users.noreply.github.com>
2022-07-26 17:38:44 -0300

    adicionado inimigos que perseguem o jogador
    por enquanto sem qualquer colisão ou funcionalidade, só seguem

--- DragStatue/core/src/com/mygdx/dragstatue/GameScreen.java
+++ DragStatue/core/src/com/mygdx/dragstatue/GameScreen.java
@@ -2,10 +2,11 @@ package com.mygdx.dragstatue;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-//import com.badlogic.gdx.graphics.g2d.TextureAtlas.TextureAtlasData.Region;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.Array;
 
 import org.w3c.dom.css.Rect;
 
@@ -20,6 +21,7 @@ public class GameScreen implements Screen {
 	Texture playerTexture;
     Texture statueTexture;
     Texture connectionTexture;
+    Texture badguysTexture;
 
     Rectangle player;
     Rectangle statue;
@@ -27,6 +29,9 @@ public class GameScreen implements Screen {
     float connectionLength;
     float connectionAngle;
 
+    Array<Rectangle> badguys;
+
+    long lastSpawnTime;
     long lastTeleportTime;
 	
 	public GameScreen(final DragStatue passed_game) {
@@ -36,6 +41,7 @@ public class GameScreen implements Screen {
 		playerTexture = new Texture("lilguy.png");
         statueTexture = new Texture("bigguy.png");
         connectionTexture = new Texture("connection.png");
+        badguysTexture = new Texture("lilbadguys.png");
 
         player = new Rectangle();
         player.width = 64;
@@ -48,6 +54,9 @@ public class GameScreen implements Screen {
         statue.y = (600 - statue.width)/2;
 
         lastTeleportTime = TimeUtils.nanoTime();
+
+        badguys = new Array<Rectangle>();
+        spawnBadguy();
 	}
 
 	@Override
@@ -65,6 +74,9 @@ public class GameScreen implements Screen {
         batch.draw(connectionTexture, statue.x+32, statue.y+32, 0, 0, connectionLength, 2, 1, 1, connectionAngle, 0, 0, 64, 64, false, false);
 		batch.draw(playerTexture, player.x, player.y); //draws the player
         batch.draw(statueTexture, statue.x, statue.y); //draws the statue
+        for (Rectangle badguy : badguys) {
+            batch.draw(badguysTexture, badguy.x, badguy.y);
+        }
 		batch.end();
 
         //receive input to move player
@@ -81,7 +93,7 @@ public class GameScreen implements Screen {
             statue.x = player.x;
             statue.y = player.y;
             lastTeleportTime = TimeUtils.nanoTime();
-        }
+    }
 
         //keep player in bounds
         if (player.x < 0) 
@@ -92,12 +104,49 @@ public class GameScreen implements Screen {
             player.y = 0;
         if (player.y > 600 - player.width) 
             player.y = 600 - player.width;
+
+        //update all the enemies
+        for (Rectangle badguy : badguys) {
+            float badguyAngle = (float) Math.atan2(player.y - badguy.y, player.x - badguy.x);
+            badguy.y += Math.sin(badguyAngle)*50*Gdx.graphics.getDeltaTime();
+            badguy.x += Math.cos(badguyAngle)*50*Gdx.graphics.getDeltaTime();
+            if (badguy.x < 0 || badguy.x > 600)
+                badguy.x = 300;
+            if (badguy.y < 0 || badguy.y > 600)
+                badguy.y = 300;
+        }
+
+        //check for enemy spawn
+        if (TimeUtils.nanoTime() - lastSpawnTime > 1000000000 && badguys.size <= 10)
+            spawnBadguy();
 	}
 	
+    private void spawnBadguy() {
+        Rectangle badguy = new Rectangle();
+        badguy.width = 64;
+        badguy.height = 64;
+
+        int spawnCoord = MathUtils.random(0, 600+600);
+        if (spawnCoord < 600){
+            badguy.x = spawnCoord;
+            badguy.y = 600;
+        }
+        else {
+            badguy.x = 600;
+            badguy.y = spawnCoord - 600;
+        }
+
+        badguys.add(badguy);
+        lastSpawnTime = TimeUtils.nanoTime();
+    }
+
 	@Override
 	public void dispose() {
-		batch.dispose();
 		playerTexture.dispose();
+        statueTexture.dispose();
+        connectionTexture.dispose();
+        badguysTexture.dispose();
+        batch.dispose();
 	}
 
     @Override
